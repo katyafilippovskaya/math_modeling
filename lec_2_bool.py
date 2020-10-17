118241c9e2810f647d6bae2ccb1144f91d41526e
katyafilippovskaya <71558066+katyafilippovskaya@users.noreply.github.com>
2020-10-17 12:48:02 +0200

    =)

---
+++ lec_2_bool.py
@@ -0,0 +1,13 @@
+print (bool(1))
+print (bool(0))
+a=3
+b=5
+print (bool(a>b))
+print (bool("Hello Vasya"))
+print (bool(""))
+print (bool([]))
+print (bool([[]]))
+
+
+
+
---
+++ lec_2_if.py
@@ -0,0 +1,9 @@
+if 1:
+    print("hello 1")
+    
+a=3
+if a>1:
+    print ("hello 3")
+b=5
+if b==5: #операция сравнения
+    print ("hello 5") 
